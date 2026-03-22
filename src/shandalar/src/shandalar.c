c6164d547fa9fc3944500dc1b9b8362f2afe095d
rler <rlerrr@gmail.com>
2026-03-22 15:01:11 -0400

    Stub out shandalar.exe

---
+++ src/shandalar/src/shandalar.c
@@ -0,0 +1,15 @@
+#include <windows.h>
+
+/*
+ * Stub executable target for shandalar.exe.
+ * Decompilation/matching work will replace this with real code.
+ */
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance, PSTR cmdLine, int nShowCmd)
+{
+  (void)hInstance;
+  (void)prevInstance;
+  (void)cmdLine;
+  (void)nShowCmd;
+  return 0;
+}
+
