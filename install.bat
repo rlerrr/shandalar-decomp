925f606fc1ac0a5a44a9f50fb98ff820ba2b7b30
rler <rlerrr@gmail.com>
2026-05-11 22:11:24 -0400

    Fix install.bat

--- install.bat
+++ install.bat
@@ -1,6 +1,7 @@
-@echo off
 setlocal
 
 cmake -S . -B out\build\nmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=RelWithDebInfo -DSHANDALAR_MODERN_FIXES=ON -DCMAKE_INSTALL_PREFIX=C:\Magic\Program
 
 cmake --build out\build\nmake
+
+cmake --install out\build\nmake
