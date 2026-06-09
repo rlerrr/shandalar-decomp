402f76fec1345fc45eaab54262eb3cb9412a1772
rler <rlerrr@gmail.com>
2026-06-09 19:38:22 -0400

    MAGIC: adjust shivan_dragon untap counter

--- src/magic/src/cards/creatures.c
+++ src/magic/src/cards/creatures.c
@@ -352,10 +352,10 @@ int card_shivan_dragon(int player, int card, event_t event)
 
   if (event == EVENT_UNTAP_PHASE)
   {
-    ++unk_00939520[player][COLOR_RED];
+    ++unk_00939530[player][0];
     return 0;
   }
-  else if (((event == EVENT_CAST_SPELL) && (affected_card == card)) && (affected_card_controller == player))
+  else if (((event == EVENT_CAST_SPELL) && (card == affected_card)) && (player == affected_card_controller))
   {
     PLAYER_CARD_INSTANCE(player, card).eot_toughness = 0;
     PLAYER_CARD_INSTANCE(player, card).info_slot = PLAYER_CARD_INSTANCE(player, card).eot_toughness;
