d60b3bcf60bae89723ffc89a13d37e5eecd8328e
rler <rlerrr@gmail.com>
2026-08-12 22:20:08 -0400

    Fix busted dupe annotations

--- src/deckdll/src/card_db.c
+++ src/deckdll/src/card_db.c
@@ -9,13 +9,11 @@ char *CsvParseNextField(char **txt);
 // GLOBAL: DECKDLL 0x100355d8
 // GLOBAL: MAGIC 0x0056eab4
 // GLOBAL: SHANDALAR 0x0058397c
-// GLOBAL: SHANDALAR 0x008e4514
 static char read_db_empty_rules[1] = "";
 
 // GLOBAL: DECKDLL 0x100355ec
 // GLOBAL: MAGIC 0x0056eac8
 // GLOBAL: SHANDALAR 0x00583990
-// GLOBAL: SHANDALAR 0x008e4518
 static char read_db_empty_flavor[1] = "";
 
 // GLOBAL: DECKDLL 0x10033e48
